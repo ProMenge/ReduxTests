47023ad099d0010e3f44ab581a5b671052f63094
Fred Menge <mengeprofissional@gmail.com>
2024-08-15 14:47:25 -0300

    :sparkles: feat: increase and decrease button functionality

--- src/components/cart-item/index.jsx
+++ src/components/cart-item/index.jsx
@@ -1,6 +1,6 @@
 import { AiOutlinePlus, AiOutlineMinus, AiOutlineClose } from "react-icons/ai";
 import { useDispatch } from "react-redux"
-import { removeProductFromCart } from "../../redux/cart/actions";
+import { removeProductFromCart, increaseProductQuantity, decreaseProductQuantity } from "../../redux/cart/actions";
 
 // Styles
 import * as Styles from "./styles";
@@ -12,9 +12,13 @@ const CartItem = ({ product }) => {
     dispatch(removeProductFromCart(product.id))
   };
 
-  const handleIncreaseClick = () => {};
+  const handleIncreaseClick = () => {
+    dispatch(increaseProductQuantity(product.id))
+  };
 
-  const handleDecreaseClick = () => {};
+  const handleDecreaseClick = () => {
+    dispatch(decreaseProductQuantity(product.id))
+  };
 
   return (
     <Styles.CartItemContainer>
--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -36,6 +36,26 @@ const cartReducer = (state = initialState, action) => {
           (product) => product.id != action.payload
         ),
       };
+    case CartActionTypes.INCREASE_PRODUCT_QUANTITY:
+      return {
+        ...state,
+        products: state.products.map((product) =>
+          product.id === action.payload
+            ? { ...product, quantity: product.quantity + 1 }
+            : product
+        ),
+      };
+    case CartActionTypes.DECREASE_PRODUCT_QUANTITY:
+      return {
+        ...state,
+        products: state.products
+          .map((product) =>
+            product.id === action.payload
+              ? { ...product, quantity: product.quantity - 1 }
+              : product
+          )
+          .filter((product) => product.quantity > 0),
+      };
     default:
       return state;
   }
